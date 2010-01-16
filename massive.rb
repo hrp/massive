133c4fddf675b6d6a99ec75a113038e7f6044b40
Hiten Parmar <hiten@hbook.local>
2010-01-15 22:38:58 -0800

    first commit

---
+++ massive.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'sinatra'
+
+def massive(word)
+  #add spaces
+  word_spaced = String.new
+  (0..word.length).each do |k|
+    word_spaced += word[k,1] + " "
+  end
+  #repeat
+  body = String.new
+  9999.times do
+    body += word_spaced
+  end
+  return body
+end
+
+get '/:word' do
+  massive(params[:word])
+end
+
+get '/' do
+  massive("MASSIVE")
+end
